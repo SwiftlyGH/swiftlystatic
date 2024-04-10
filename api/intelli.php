dbc3ff9698dd53aa33cbfcdcd1989f191fb4596d
Pico_190 <86069057+pico190@users.noreply.github.com>
2024-04-10 17:51:07 +0200

    + Intelli

---
+++ api/intelli.php
@@ -0,0 +1,11 @@
+<?php
+header('Access-Control-Allow-Origin: *');
+header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
+header("Allow: GET, POST, OPTIONS, PUT, DELETE");
+
+header('Content-Type: application/json; charset=utf-8');
+$syntax = str_replace("..", "", $_GET["syntax"]);
+$lang = $_GET["lang"];
+
+echo file_get_contents("../airacloud/references/$syntax/$lang.json");
