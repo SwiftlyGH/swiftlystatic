643ca63b3a57f572e22f3a42cbbc12bc99d79745
Pico_190 <86069057+pico190@users.noreply.github.com>
2024-04-10 18:08:56 +0200

    + Error info

--- api/intelli.php
+++ api/intelli.php
@@ -1,13 +1,17 @@
 <?php
+try {
+ 
+ini_set('display_errors', 0);
 header('Access-Control-Allow-Origin: *');
 header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method");
 header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
 header("Allow: GET, POST, OPTIONS, PUT, DELETE");
 
-
 header('Content-Type: application/json; charset=utf-8');
-echo json_encode($_GET)
 $syntax = str_replace("..", "", $_GET["syntax"]);
-$lang = $_GET["lang"];
+$lang = str_replace("..", "", $_GET["lang"]);
 
-echo file_get_contents("../airacloud/references/$syntax/$lang.json");
+echo file_get_contents("../airacloud/references/$syntax/$lang.json");   
+} catch(Error $ERR) {
+    print_r($ERR);
+}
