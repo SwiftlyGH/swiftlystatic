eacd157fbf2aed15b4ac01d1cbc5cf0508c711dc
Pico_190 <86069057+pico190@users.noreply.github.com>
2024-04-10 17:57:43 +0200

    - Errors

--- api/intelli.php
+++ api/intelli.php
@@ -1,4 +1,5 @@
 <?php
+ini_set('display_errors', 0);
 header('Access-Control-Allow-Origin: *');
 header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method");
 header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
