b7eeb30b4822298df4c854f03dabc8d736aa4e3c
Pico_190 <86069057+pico190@users.noreply.github.com>
2024-04-10 18:05:52 +0200

    * Get is not working.

--- api/intelli.php
+++ api/intelli.php
@@ -1,12 +1,13 @@
 <?php
-ini_set('display_errors', 0);
 header('Access-Control-Allow-Origin: *');
 header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method");
 header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
 header("Allow: GET, POST, OPTIONS, PUT, DELETE");
 
+
 header('Content-Type: application/json; charset=utf-8');
+echo json_encode($_GET)
 $syntax = str_replace("..", "", $_GET["syntax"]);
-$lang = str_replace("..", "", $_GET["lang"]);
+$lang = $_GET["lang"];
 
 echo file_get_contents("../airacloud/references/$syntax/$lang.json");
