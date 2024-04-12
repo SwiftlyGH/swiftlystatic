aa6995f16fe74a65d7b94566313c11fe3fa89bf8
pico190 <saulbuesosanz@gmail.com>
2024-04-12 18:23:38 +0200

    works :D

--- api/airacloud-icon.php
+++ api/airacloud-icon.php
@@ -1,4 +1,11 @@
 <?php
+    ini_set('display_errors', 0);
+
+    // Set appropriate CORS headers
+    header('Access-Control-Allow-Origin: *');
+    header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method");
+    header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
+    header("Allow: GET, POST, OPTIONS, PUT, DELETE");
 
 header('Content-type: image/svg+xml');
 
